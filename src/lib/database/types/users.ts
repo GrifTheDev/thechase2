7d0e41292c023552ecb09b46e354c0d8cc83ffbd
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2024-10-23 22:46:38 +0200

    started designing the login page gn

--- src/lib/database/types/users.ts
+++ src/lib/database/types/users.ts
@@ -1,6 +1,7 @@
 // Even though a DB query for users must always return all of the interface properties, the "?"
 // has been added so that I can write to this collection without having to express all other
 // properties of the document.
+
 interface DBUsersType {
     name?: string,
     surname?: string,
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,16 @@
+<!--TODO: THIS MUST BE MADE INTO REUSABLE COMPONENTS AFTER THE DESIGN IS COMPLETE-->
+<div class="h-screen flex flex-col items-center justify-center bg-purple-200">
+        <form class="rounded-lg border-black border-2 w-1/3 h-2/3 flex flex-col items-center justify-center bg-blue-100" method="POST">
+            <h1 class="text-5xl font-bold m-3 text-center">Welcome back!</h1>
+            <label class="w-full flex flex-col items-center justify-center">
+                <p class="text-center text-xl font-thin">Email</p>
+                <input class="border border-gray-900 rounded-sm p-1 w-3/5 text-lg" name="email" type="email">
+            </label>
+            <label class="w-full flex flex-col items-center justify-center">
+                <p class="text-center text-xl font-thin">Password</p>
+                <input class="border border-gray-900 rounded-sm p-1 w-3/5 text-lg" name="password" type="password">
+            </label>
+            <button class="text-xl font-bold border rounded-md border-black p-2 m-5 w-2/5">Login</button>
+        </form>
+        
+</div>
