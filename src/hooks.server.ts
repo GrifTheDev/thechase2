eb14121b1731fd2269645f1dd2e29483b80b2908
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2024-10-29 22:58:01 +0100

    Add route protection via middleware & change hashing algorithm.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,12 @@
 import { PRIVATE_JWT_SECRET } from "$env/static/private";
 import type { AuthCookieType } from "$lib/types/misc/auth_cookie";
-import type { Handle } from "@sveltejs/kit";
+import { redirect, type Handle } from "@sveltejs/kit";
 import jwt from "jsonwebtoken";
 
 export const handle = (async ({ event, resolve }) => {
   const token = event.cookies.get("AuthorizationToken");
 
+  
   if (token != undefined) {
     try {
         const userInfo = jwt.verify(token, PRIVATE_JWT_SECRET) as AuthCookieType
@@ -13,10 +14,22 @@ export const handle = (async ({ event, resolve }) => {
         event.locals.user = userInfo
 
     } catch (error) {
-        // !!TEST POSSIBLE CASES WHERE THIS HITS
+      // !!TEST POSSIBLE CASES WHERE THIS HITS
+      // !!TEST POSSIBLE CASES WHERE THIS HITS
+      // !!TEST POSSIBLE CASES WHERE THIS HITS
+      // !!NEXT PRIORITY TASK, CHECK AND HANDLE JWT SIGNAGE FAILED
+        if (event.url.pathname.split('/').includes("app")) {
+          event.cookies.delete("AuthorizationToken", {
+            path: "/"
+          })
+          throw redirect(303, "/login")
+        }
+        
         // ? WHAT HAPPENS WHEN JWT.VERIFY FAILS?
         console.error(error)
     }
+  } else {
+    if (event.url.pathname.split('/').includes("app")) throw redirect(303, "/login")
   }
 
   return await resolve(event);
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -6,9 +6,10 @@ import jwt from "jsonwebtoken"
 import { PRIVATE_JWT_SECRET } from "$env/static/private";
 import type { AuthCookieType } from "$lib/types/misc/auth_cookie";
 import type { PageServerLoad } from "./$types";
+import { createConstantSaltHash } from "$lib/server/auth";
 
 export const load: PageServerLoad = async ({locals}) => {
-    //console.log(locals.user)
+    console.log(locals.user)
     if (locals.user != undefined) throw redirect(303, "/")
 }; 
 
@@ -28,10 +29,8 @@ export const actions = {
     // The part after @ in the email does not contain a .
     if (email.split("@")[1].split(".").length < 2)
       return { code: 400, message: "Invalid e-mail." };
-
-    // !! CHANGE EMAIL HASH TO NEW THING
-    // left comment cus i dont have time to properly test it rn
-    const emailHash = createHash("sha256").update(email).digest("hex");
+    
+    const emailHash = await createConstantSaltHash(email)
 
     const dbData = await readUsersData(emailHash);
 
