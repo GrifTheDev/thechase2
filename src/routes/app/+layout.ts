764930977c25cecb691f5512c1b01c71b9bbbc21
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2024-10-30 22:38:26 +0100

    trying to get access to locals, failing for now, gn

--- src/routes/app/+layout.ts
+++ src/routes/app/+layout.ts
@@ -1,9 +1,9 @@
 import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = ({ url }) => {
+export const load: LayoutLoad = ({ url, data }) => {
     const endpoint = url.pathname
     if (endpoint.startsWith("/app/dashboard")) return {selected: [1, 0, 0]}
     if (endpoint.startsWith("/app/questions")) return {selected: [0, 1, 0]}
     if (endpoint.startsWith("/app/account")) return {selected: [0, 0, 1]}
-    return {selected: [0, 0, 0]}
+    return {selected: [0, 0, 0], data}
 };
---
+++ src/routes/app/dashboard/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+
+    let {data} = $props()
+</script>
+
+<h1 class="text-3xl font-bold">Hello {data.data?.user.name}!</h1>
