100611726de9645b4c2c473d19e7acbaa6900b8b
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2024-10-31 23:44:27 +0100

    Finished the initial design template - #9

---
+++ src/routes/design/+page.svelte
@@ -0,0 +1,12 @@
+<!--TODO Put these into reusable components.-->
+<div class="bg-dashboard-bg w-screen h-screen">
+  <div class="p-6 flex flex-col space-y-4 items-start border-b border-white">
+    <h1 class="font-dm-sans font-bold text-5xl text-white">Welcome Teo!</h1>
+    <h1 class="font-dm-sans text-4xl text-white">Question Sets</h1>
+    <p class="text-white text-lg">You do not have any question sets yet.</p>
+    <button
+      class="text-white text-center border border-white rounded-md p-2 animate-buttonUnHover hover:animate-buttonHover active:animate-buttonActivate"
+      >Create Question Set</button
+    >
+  </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,29 @@ export default {
       colors: {
         "nav-bg": "#070f1c",
         "dashboard-bg": "#070f1c",
-      }
+      },
+      fontFamily: {
+        "dm-sans": ["DMSans", "sans-serif"]
+      },
+      keyframes: {
+        buttonHoverKeyFrames: {
+          '0%': { "transform": 'scale(1)', "background-color": "transparent", "color": "white"},
+          '100%': { "transform": 'scale(1.05) rotate(0.7deg)', "background-color": "white", "color": "black"},
+        },
+        buttonUnHoverKeyFrames: {
+          '0%': { "transform": 'scale(1.05) rotate(0.7deg)', "background-color": "white", "color": "black"  },
+          '100%': { "transform": 'scale(1)', "background-color": "transparent", "color": "white" },
+        },
+        buttonActivateKeyframes: {
+          '0%': { transform: 'scale(1.05) rotate(0.7deg)' },
+          '100%': { transform: 'scale(0.95) rotate(-0.7deg)' },
+        }
+      },
+      animation: {
+        buttonHover: "buttonHoverKeyFrames 0.3s ease-in-out forwards",
+        buttonUnHover: "buttonUnHoverKeyFrames 0.3s ease-in-out forwards",
+        buttonActivate: "buttonActivateKeyframes 0.15s ease-in-out forwards"
+      },
     },
   },
   plugins: [],
