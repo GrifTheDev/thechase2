444b7741005bf7947c10ac0258e5f55bb80b3e1a
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2025-01-07 22:29:31 +0100

    Slight change to game table type

--- src/lib/types/database/games.ts
+++ src/lib/types/database/games.ts
@@ -2,14 +2,20 @@ import type { HunterObjectType } from "../misc/hunter_object";
 import type { PlayerTeamObjectType } from "../misc/player_team_object";
 
 interface DBGameType {
-    game_id: string,
+    game: {
+        id: string,
+        state: number,
+    },
     question_set_id: string,
     player_teams: Array<PlayerTeamObjectType>,
     hunter: HunterObjectType
 }
 
 interface DBGameTypeWrite {
-    game_id?: string,
+    game?: {
+        id: string,
+        state: number,
+    },
     question_set_id?: string,
     player_teams?: Array<PlayerTeamObjectType>,
     hunter?: HunterObjectType
